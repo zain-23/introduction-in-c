b997be689d06976821ba31413c65746d79f458a5
Syed zain ali shah <zaingit780@gmail.com>
2024-10-14 16:02:48 +0500

    learn if statement

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c = 'Y';
+    if(c == 'y' || c == 'Y')
+    {
+        printf("Agreed!\n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("Not Agreed!\n");
+    }
+}
